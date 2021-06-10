28ec3b58debe7d7827542cf21dfa0a1f8d4a34b3
JYang <947405980@qq.com>
2021-06-10 21:52:29 +0800

    feat：新增刷新图库视频方法（未完善）

--- app/src/main/java/com/yk/mediademo/utils/AlbumUtils.java
+++ app/src/main/java/com/yk/mediademo/utils/AlbumUtils.java
@@ -32,11 +32,43 @@ public class AlbumUtils {
         context.sendBroadcast(intent);
     }
 
+    public static void notifyAlbumUpdateVideo(Context context, String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            return;
+        }
+
+        ContentResolver contentResolver = context.getContentResolver();
+
+        ContentValues contentValues = getVideoContentValues(file);
+
+        contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(file);
+        intent.setData(uri);
+        context.sendBroadcast(intent);
+    }
+
     private static ContentValues getImageContentValues(File file) {
         ContentValues contentValues = new ContentValues();
         contentValues.put(MediaStore.Images.Media.TITLE, file.getName());
         contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, file.getName());
         contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        contentValues.put(MediaStore.Images.Media.DATE_MODIFIED, System.currentTimeMillis());
+        contentValues.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+        contentValues.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
+        contentValues.put(MediaStore.Images.Media.SIZE, file.length());
+
+        return contentValues;
+    }
+
+    private static ContentValues getVideoContentValues(File file) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, file.getName());
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, file.getName());
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/mp4");
         contentValues.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
         contentValues.put(MediaStore.Images.Media.DATE_MODIFIED, System.currentTimeMillis());
         contentValues.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
