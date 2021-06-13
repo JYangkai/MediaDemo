93394456c202c2427273b580d9736a33d9f3f07b
JYang <947405980@qq.com>
2021-06-13 14:53:55 +0800

    feat：增加滤镜选择的颜色

--- app/src/main/java/com/yk/mediademo/data/adapter/FilterAdapter.java
+++ app/src/main/java/com/yk/mediademo/data/adapter/FilterAdapter.java
@@ -1,6 +1,7 @@
 package com.yk.mediademo.data.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,8 @@ public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.ViewHolder
     private Context context;
 
     private final List<BaseRenderBean> filterList;
+
+    private int curClickIndex = -1;
 
     public FilterAdapter(List<BaseRenderBean> filterList) {
         this.filterList = filterList;
@@ -39,6 +42,7 @@ public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.ViewHolder
                 if (onClickFilterListener != null) {
                     onClickFilterListener.onClickFilter(filter);
                 }
+                updateSelect(position);
             }
         });
         return holder;
@@ -48,11 +52,29 @@ public class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.ViewHolder
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         BaseRenderBean filter = filterList.get(position);
         holder.tvName.setText(filter.getName());
+        if (position == curClickIndex) {
+            holder.tvName.setTextColor(Color.RED);
+        } else {
+            holder.tvName.setTextColor(Color.WHITE);
+        }
     }
 
     @Override
     public int getItemCount() {
         return filterList.size();
+    }
+
+    private void updateSelect(int position) {
+        if (curClickIndex == -1) {
+            curClickIndex = position;
+            notifyItemChanged(position);
+            return;
+        }
+
+        int lastClickIndex = curClickIndex;
+        curClickIndex = position;
+        notifyItemChanged(lastClickIndex);
+        notifyItemChanged(curClickIndex);
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
