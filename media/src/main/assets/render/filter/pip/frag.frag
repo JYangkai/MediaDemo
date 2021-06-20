1cc41b3e8f274e038d4a8c39e7b92eac8fb114cc
JYang <947405980@qq.com>
2021-06-20 09:31:58 +0800

    fix：优化画中画滤镜

--- media/src/main/assets/render/filter/pip/frag.frag
+++ media/src/main/assets/render/filter/pip/frag.frag
@@ -2,14 +2,10 @@ precision mediump float;
 uniform sampler2D uSampler;
 uniform sampler2D uSampler2;
 varying vec2 vCoordinate;
-const float SCALE = 2.0;
 void main(){
-    float scaleX = (2.0 * vCoordinate.x + SCALE - 1.0) / (2.0 * SCALE);
-    float scaleY = (2.0 * vCoordinate.y + SCALE - 1.0) / (2.0 * SCALE);
-
     if (vCoordinate.y > 0.33 && vCoordinate.y < 0.66) {
         gl_FragColor = texture2D(uSampler, vCoordinate);
     } else {
-        gl_FragColor = texture2D(uSampler2, vec2(scaleX, scaleY));
+        gl_FragColor = texture2D(uSampler2, vCoordinate);
     }
 }
--- media/src/main/java/com/yk/media/opengles/render/filter/PipFilter.java
+++ media/src/main/java/com/yk/media/opengles/render/filter/PipFilter.java
@@ -4,14 +4,16 @@ import android.content.Context;
 import android.opengl.GLES20;
 
 import com.yk.media.opengles.render.bean.filter.GaussianBlurBean;
+import com.yk.media.opengles.render.bean.filter.ScaleBean;
 import com.yk.media.utils.FilterUtils;
 
 public class PipFilter extends BaseFilter {
     private int uSampler2Location;
 
-    private int blurTextureId;
+    private int scaleBlurTextureId;
 
     private GaussianBlurFilter gaussianBlurFilter;
+    private ScaleFilter scaleFilter;
 
     public PipFilter(Context context) {
         super(
@@ -21,7 +23,10 @@ public class PipFilter extends BaseFilter {
         );
 
         gaussianBlurFilter = (GaussianBlurFilter) FilterUtils.getFilter(
-                context, new GaussianBlurBean(2, 30,3,3)
+                context, new GaussianBlurBean(2, 30, 3, 3)
+        );
+        scaleFilter = (ScaleFilter) FilterUtils.getFilter(
+                context, new ScaleBean("放大", 2)
         );
     }
 
@@ -29,18 +34,21 @@ public class PipFilter extends BaseFilter {
     public void onCreate() {
         super.onCreate();
         gaussianBlurFilter.onCreate();
+        scaleFilter.onCreate();
     }
 
     @Override
     public void onChange(int width, int height) {
         super.onChange(width, height);
         gaussianBlurFilter.onChange(width, height);
+        scaleFilter.onChange(width, height);
     }
 
     @Override
     public void onDraw(int textureId) {
         gaussianBlurFilter.onDraw(textureId);
-        blurTextureId = gaussianBlurFilter.getFboTextureId();
+        scaleFilter.onDraw(gaussianBlurFilter.getFboTextureId());
+        scaleBlurTextureId = scaleFilter.getFboTextureId();
         super.onDraw(textureId);
     }
 
@@ -54,7 +62,7 @@ public class PipFilter extends BaseFilter {
     public void onActiveTexture(int textureId) {
         super.onActiveTexture(textureId);
         GLES20.glActiveTexture(GLES20.GL_TEXTURE1);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, blurTextureId);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, scaleBlurTextureId);
         GLES20.glUniform1i(uSampler2Location, 1);
     }
 }
